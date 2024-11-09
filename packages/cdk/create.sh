0559d1f53a47d8c6102683e796749cece691ab91
Kaito Udagawa <umireon@gmail.com>
2024-11-10 08:21:08 +0900

    Update create.sh

--- packages/cdk/create.sh
+++ packages/cdk/create.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname "$0")" || exit 1
 
-npx cdk bootstrap --trust 147997151289
+npx cdk bootstrap --trust 147997151289 --cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess
