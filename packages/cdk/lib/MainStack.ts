fe16ba93f14707ace4beffc560c68ee0768c3d44
Kaito Udagawa <umireon@gmail.com>
2024-11-10 16:57:21 +0900

    Fix

--- packages/cdk/lib/MainStack.ts
+++ packages/cdk/lib/MainStack.ts
@@ -1,12 +1,40 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 
-import { aws_apigatewayv2 as apigwv2, aws_lambda_nodejs as nodejs } from "aws-cdk-lib";
+import {
+	aws_apigatewayv2 as apigwv2,
+	aws_iam as iam,
+	aws_lambda_nodejs as nodejs,
+	aws_route53 as route53
+} from "aws-cdk-lib";
 import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
 
 export class MainStack extends cdk.Stack {
 	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
+
+		const apiDevZone = new route53.PublicHostedZone(this, "ApiDevZone", {
+			zoneName: "apidev.obs-chattalker.kaito.tokyo"
+		});
+
+		const delegationRoleArn = this.formatArn({
+			region: "",
+			service: "iam",
+			account: "913524900670",
+			resource: "role",
+			resourceName: "MyDelegationRole"
+		});
+		const delegationRole = iam.Role.fromRoleArn(
+			this,
+			"ApiDevZoneDelegationRole",
+			delegationRoleArn
+		);
+
+		new route53.CrossAccountZoneDelegationRecord(this, "ApiDevZoneDelegate", {
+			delegatedZone: apiDevZone,
+			parentHostedZoneName: "obs-chattalker.kaito.tokyo",
+			delegationRole
+		});
 
 		const oauthCallbackFunction = new nodejs.NodejsFunction(this, "OauthCallbackFunction", {
 			entry: "lib/lambda/entrypoints.ts",
