32fada1bde0e352b498c4b02ad907663e77d4e62
Kaito Udagawa <umireon@gmail.com>
2024-11-11 06:37:19 +0900

    Update RecursiveDefaultRoot.js

--- packages/cdk/cloudfront/RecursiveDefaultRoot.js
+++ packages/cdk/cloudfront/RecursiveDefaultRoot.js
@@ -4,6 +4,8 @@
  */
 async function handler(event) {
 	const request = event.request;
-	request.uri += "/index.html";
+    if (!request.uri.includes(".")) {
+	    request.uri += "/index.html";
+    }
 	return request;
 }
