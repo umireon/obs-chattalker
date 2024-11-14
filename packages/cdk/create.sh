9a16df74d0cfc3962cd7dfdb26ed48855723292f
Kaito Udagawa <umireon@gmail.com>
2024-11-14 23:51:53 +0900

    Fix

--- packages/cdk/create.sh
+++ packages/cdk/create.sh
@@ -2,4 +2,6 @@
 
 cd "$(dirname "$0")" || exit 1
 
-npx aws-cdk bootstrap --trust 147997151289 --cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess
+npm run cdk -- bootstrap \
+  --trust arn:aws:iam::147997151289:role/ObsChatTalkerMainRole \
+  --cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess
