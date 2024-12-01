3b43d6f497c263becf5fbfeb3c6b5174e17876d9
Kaito Udagawa <umireon@gmail.com>
2024-12-01 22:44:35 +0900

    Update create.sh

--- packages/cdk/create.sh
+++ packages/cdk/create.sh
@@ -2,6 +2,6 @@
 
 cd "$(dirname "$0")" || exit 1
 
-npm run cdk -- bootstrap \
-  --trust arn:aws:iam::147997151289:role/ObsChatTalkerMainRole \
-  --cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess
+npm run cdk -- bootstrap aws://586794439382/us-east-1 \
+  --trust=arn:aws:iam::147997151289:role/Builders/ObsChatTalkerBuilderCodeBuildRole \
+  --cloudformation-execution-policies=arn:aws:iam::aws:policy/AdministratorAccess
